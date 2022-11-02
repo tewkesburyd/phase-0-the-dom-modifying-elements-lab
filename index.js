a53658bab088a83ddc6f5d23d52321b7f5dc50f0
David Tewkesbury <tewkesburyd@hotmail.com>
2022-11-01 23:01:42 -0600

    commit tree 065071a442c7f7479fb1f2386563f89525345780

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const removeMain = document.querySelector('#main')
+removeMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'David is the champion';
